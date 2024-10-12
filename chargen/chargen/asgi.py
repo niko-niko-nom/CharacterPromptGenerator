92a0900503c32fc3e9616d599e345421024d08a4
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-10-12 13:53:32 +0200

    Django installed succesfully

---
+++ chargen/chargen/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for chargen project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chargen.settings')
+
+application = get_asgi_application()
