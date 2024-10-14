c4a0d6d2a8b14c9ae2eca9e15206df2f166f5c6e
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-10-14 17:06:25 +0200

    Implemented account deletion

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from allauth.account.views import LoginView
+from django.contrib import messages
 
 
 class SignUpView(CreateView):
@@ -13,3 +15,14 @@ class SignUpView(CreateView):
 class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse("library", username=self.request.user.username)
+    
+@login_required
+def delete_account(request):
+    user = request.user
+
+    if request.method == 'POST':
+        user.delete()
+        messages.success(request, "Your account has been deleted.")
+        return redirect(reverse('account_logout'))
+    
+    return render(request, "account/delete_account.html")
--- chargen/urls.py
+++ chargen/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from allauth.account.views import LoginView
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,5 @@ urlpatterns = [
     path("<str:username>/library/", TemplateView.as_view(template_name="library.html"), name="library"),
     path("accounts/login/", LoginView.as_view(), name="account_login"),
     path("accounts/", include("allauth.urls")),
+    path('delete-account/', views.delete_account, name='delete_account'),
 ]
