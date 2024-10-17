90cc747e0f1f4c3f83d2cf94713f3b1a649c2fa3
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-10-17 17:30:44 +0200

    Nuked database, re-created superuser and made (hopefully working) prompt, subcategory and category tables

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from .models import Category, Subcategory, Prompt
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -12,3 +12,32 @@ class CustomUserAdmin(UserAdmin):
     list_display = ["email", "username",]
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+class SubcategoryInline(admin.TabularInline):
+    model = Subcategory
+    extra = 1
+    fields = ['name', 'order']  # Show order field for sorting
+    ordering = ['order']
+
+class PromptInline(admin.TabularInline):
+    model = Prompt
+    extra = 1
+    fields = ['text', 'order']  # Show order field for sorting
+    ordering = ['order']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'order']
+    inlines = [SubcategoryInline]
+    ordering = ['order']
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'order']
+    inlines = [PromptInline]
+    ordering = ['order']
+
+@admin.register(Prompt)
+class PromptAdmin(admin.ModelAdmin):
+    list_display = ['text', 'subcategory', 'order']
+    ordering = ['order']
--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,40 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # add additional fields in here
 
     def __str__(self):
         return self.email
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    order = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['order']  # Ensure ordering by 'order' field
+
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    order = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.category.name} -> {self.name}'
+
+    class Meta:
+        ordering = ['order']  # Ensure ordering by 'order' field
+
+
+class Prompt(models.Model):
+    subcategory = models.ForeignKey(Subcategory, related_name='prompts', on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    order = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.subcategory.name}: {self.text}'
+
+    class Meta:
+        ordering = ['order']  # Ensure ordering by 'order' field
