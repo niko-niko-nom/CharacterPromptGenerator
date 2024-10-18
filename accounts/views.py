669d298087cbc10d54523c884c5cb6162fe3f7f4
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-10-18 17:39:30 +0200

    Nuked database again, re-re-created superuser and made new tables

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,15 @@
-from django.urls import reverse_lazy, reverse
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView
-from django.shortcuts import redirect, render
 from allauth.account.views import LoginView, EmailView
-from django.contrib import messages
-
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+from .models import Category, Subcategory, Prompt
 
 class CustomLoginView(LoginView):
     def get_success_url(self):
-        return reverse("library", username=self.request.user.username)
-    
+        return reverse("library", kwargs={"username": self.request.user.username})
+
 @login_required
 def delete_account(request):
     user = request.user
@@ -33,3 +27,17 @@ class CustomEmailChangeView(LoginRequiredMixin, EmailView):
     def form_valid(self, form):
         form.save()
         return redirect(reverse("account_email"))
+
+def home(request):
+    categories = Category.objects.all()
+    subcategories = Subcategory.objects.all()
+    prompts = Prompt.objects.all()
+    return render(request, 'home.html', {
+        'categories': categories,
+        'subcategories': subcategories,
+        'prompts': prompts
+    })
+
+def prompt_view(request):
+    categories = Category.objects.prefetch_related('subcategories__prompts').order_by('order')
+    return render(request, 'home.html', {'categories': categories})
