0f653c2422cb2381e878edd5d1f8c299089b2c04
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-10-20 10:23:51 +0200

    Add default value for user field in Prompt model

--- accounts/models.py
+++ accounts/models.py
@@ -32,7 +32,7 @@ class Prompt(models.Model):
     text = models.CharField(max_length=200)
     order = models.PositiveIntegerField(default=0)
     probability = models.FloatField(default=1.0)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default="1")
 
     def __str__(self):
         return self.text
